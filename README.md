c2c9e5ed7dba90b1be3fe95bdb09c6b085febf0b
Brian <brian.pfeil@gmail.com>
2014-05-10 15:07:04 -0400

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+## forcetk-additions
+
+extensions to the [ForceTK](https://github.com/developerforce/Force.com-JavaScript-REST-Toolkit) javascript library
+
+### Additional Methods
+
+```queryAll```
---
+++ forcetk-additions.js
@@ -0,0 +1,37 @@
+/**
+ * forcetk-additions.js
+ * depends on forcetk.js - https://github.com/developerforce/Force.com-JavaScript-REST-Toolkit/forcetk.js
+ */
+
+// fetches all results even if >2000 records returned from query
+forcetk.Client.prototype.queryAll = function(soql, callback, error) {
+    var that = this;
+
+    // first level call if previous result not on callback
+    if (callback.result) {
+
+        return that.query(soql, function(result) {
+            if (result.done) {
+                callback(result);
+            } else {
+                callback.result = result;
+                that.queryAll(soql, callback, error);
+            }
+        }, error);
+
+        
+    } else {
+
+        return that.queryMore(callback.result.nextRecordsUrl, function(result) {
+            callback.result.records = callback.result.records.concat(result.records);
+            if (result.done) {
+                callback(callback.result);
+            } else {
+                callback.result.nextRecordsUrl = result.nextRecordsUrl;
+                that.queryAll(soql, callback, error);
+            }
+            
+        }, error );
+        
+    }         
+}
