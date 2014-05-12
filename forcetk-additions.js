fe947239845a245a3359cf948a3eb55c2b3d613c
Brian <brian.pfeil@gmail.com>
2014-05-12 11:16:41 -0400

    added userid method to fetch the current user's userid

--- forcetk-additions.js
+++ forcetk-additions.js
@@ -3,6 +3,20 @@
  * depends on forcetk.js - https://github.com/developerforce/Force.com-JavaScript-REST-Toolkit/forcetk.js
  */
 
+// fetches the current user's userid
+// leverages the resources to get the users identify URI and
+// parse the userid from it
+// e.g. identity URI: https://test.salesforce.com/id/00DZ000000N9pRiMAJ/005Z0000001Z905IAC
+// orgId: 00DZ000000N9pRiMAJ
+// userid: 005Z0000001Z905IAC
+forcetk.Client.prototype.userid = function(callback, error) {
+    return this.resources(function(result) {
+        var identityURI = result['identity'];
+        var userid = identityURI.substring(identityURI.lastIndexOf('/') + 1);
+        callback(userid);     
+    }, error);
+}
+
 // fetches all results even if >2000 records returned from query
 forcetk.Client.prototype.queryAll = function(soql, callback, error) {
     var that = this;
